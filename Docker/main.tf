d2b6d35caa65ceb7fe09467bd3c6a25fefafc355
MrPinut <50791972+MrPinut@users.noreply.github.com>
2022-11-30 17:09:14 +0100

    2 eme commit
    2

--- Docker/main.tf
+++ Docker/main.tf
@@ -2,3 +2,6 @@ provider "docker" {
   # Configuration options
   host = "npipe:////.//pipe//docker_engine"
 }
+provider "google" {
+  # Configuration options
+}
--- Docker/terraform.tf
+++ Docker/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source = "kreuzwerker/docker"
       version = "2.23.1"
     }
+    google = {
+      source = "hashicorp/google"
+      version = "4.44.1"
+    }
   }
 }
 
